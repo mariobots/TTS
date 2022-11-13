241952f25c3a9a18f756d014ebc0ad761c3fe8a6
mariobots <117969657+mariobots@users.noreply.github.com>
2022-11-13 16:40:18 -0600

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from gtts import gTTS
 from random import choice
-from playsound import playsound
+from pygame import mixer
+# from playsound import playsound
 from time import sleep
 
 text = "The red zone is for immediate loading and unloading of passengers only. There is no stopping in a white zone."
@@ -13,5 +14,10 @@ tts = gTTS(text, tld=accent, slow=slow)
 tts.save("main.mp3")
 
 
-playsound('main.mp3')
+mixer.init()
+mixer.music.load("main.mp3")
+mixer.music.play()
+while mixer.music.get_busy() == True:
+    continue
+# playsound('main.mp3')
 sleep(3)
