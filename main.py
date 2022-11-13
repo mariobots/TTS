6782aa40a0f22cead4c9409bec9308b6fa4244d3
m-bots <117969657+mariobots@users.noreply.github.com>
2022-11-13 02:46:17 -0600

    added speed toggle

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from random import choice, random
 from playsound import playsound
 
 text = "The red zone is for immediate loading and unloading of passengers only. There is no stopping in a white zone."
-
+slow = False
 
 accents = ["com", "com.au", "co.uk", "ie", "co.in", "co.za"]
 accent = choice(accents)
-tts = gTTS(text, lang='en', tld=accent)
+
+tts = gTTS(text, lang='en', tld=accent, slow=slow)
 tts.save("main.mp3")
 
 
