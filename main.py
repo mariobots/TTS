2d0b57498789925458f8f3d0b240adaa1a07d435
m-bots <117969657+mariobots@users.noreply.github.com>
2022-11-13 02:26:57 -0600

    added playback

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from gtts import gTTS
 from random import choice, random
+from playsound import playsound
 
 text = "The red zone is for immediate loading and unloading of passengers only. There is no stopping in a white zone."
 
 
-accents = ["com","com.au","co.uk","ie","co.in", "co.za"]
+accents = ["com", "com.au", "co.uk", "ie", "co.in", "co.za"]
 accent = choice(accents)
 tts = gTTS(text, lang='en', tld=accent)
 tts.save("main.mp3")
 
 
+playsound('main.mp3')
